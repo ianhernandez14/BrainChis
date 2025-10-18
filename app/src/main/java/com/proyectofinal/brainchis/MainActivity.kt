112e284dee1b32c00d8d23f844f34d115ae8407d
Ian <al346169@edu.uaa.mx>
2025-10-18 17:55:20 -0600

    Mejora al splash y al menú

--- app/src/main/java/com/proyectofinal/brainchis/MainActivity.kt
+++ app/src/main/java/com/proyectofinal/brainchis/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_ayuda)
+        setContentView(R.layout.activity_juego)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
